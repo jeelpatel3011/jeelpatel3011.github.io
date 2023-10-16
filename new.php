065fa9d40a8dc882bd5977973104f4b54678eba0
jeelpatel3011 <jeelp3011@gmail.com>
2023-10-16 18:09:44 +0530

    merge new.php

--- new.php
+++ new.php
@@ -1,9 +1,6 @@
 <?php
-<<<<<<< HEAD
 echo "this is new file changed thrice";
 echo "emergency bug is fixed";
 echo "This line is here to show how merging works.";
-=======
 echo "this is new file";
->>>>>>> demo
 ?>
