cdfab6800a65eb6cb04606dd260f47a25fa6d16a
Jeel Patel <82766018+jeelpatel3011@users.noreply.github.com>
2023-10-17 11:24:13 +0530

    Added line in new.php
    "this is file from github-demo branch" line added

--- new.php
+++ new.php
@@ -3,4 +3,5 @@ echo "this is new file changed thrice";
 echo "emergency bug is fixed";
 echo "This line is here to show how merging works.";
 echo "this is new file";
+echo "this is file from github-demo branch";
 ?>
