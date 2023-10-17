5a5c58de6bc3cb7a88e6d61d1171f65f24d349e8
Jeel Patel <82766018+jeelpatel3011@users.noreply.github.com>
2023-10-17 15:34:35 +0530

    all lines removed new.php

--- new.php
+++ new.php
@@ -1,7 +1,3 @@
 <?php
-echo "this is new file changed thrice";
-echo "emergency bug is fixed";
-echo "This line is here to show how merging works.";
-echo "this is new file";
-echo "this is file from github-demo branch";
+
 ?>
