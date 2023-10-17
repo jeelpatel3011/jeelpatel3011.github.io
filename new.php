b87c252efa11a0bc47d1b3f98ebe35cdbd848099
Jeel Patel <82766018+jeelpatel3011@users.noreply.github.com>
2023-10-17 12:02:43 +0530

    Line added new.php

--- new.php
+++ new.php
@@ -1,4 +1,7 @@
 <?php
 echo "this is new file changed thrice";
-
+echo "emergency bug is fixed";
+echo "This line is here to show how merging works.";
+echo "this is new file";
+echo "this is file from github-demo branch";
 ?>
